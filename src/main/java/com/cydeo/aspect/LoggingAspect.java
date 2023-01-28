250b716cfbcf6eef260ab667ba1e59019fcfaaf6
DilaraAsl <dcavus@gmail.com>
2023-01-28 13:05:09 -0800

    committing logging aspect

---
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -0,0 +1,15 @@
+package com.cydeo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    Logger logger= LoggerFactory.getLogger(LoggingAspect.class);
+
+
+}
--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,18 +1,45 @@
 package com.cydeo.controller;
 
+import com.cydeo.annotation.ExecutionTime;
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @Tag(name="UserController",description = "User API") //swagger tag
 public class UserController {
     private final UserService userService;// if there were more than one class under UserService for ex: UserServiceImpl1 &UserServiceImpl2
-    // then we need to give precedence to one by add @Qualifier or @Primary with @Service annotation
+    // then we need to give precedence to one by add  @Primary at class level  with @Service annotation
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @ExecutionTime // this is our annotation
+    @GetMapping
+    @RolesAllowed({"Manager","Admin"}) //JAX-RS annotation
+    public ResponseEntity<ResponseWrapper> getUsers(){ // ResponseEntity represents the whole HTTP response: status code, headers, and body
+        // we use it to fully configure the HTTP response
+        //ResponseEntity is a generic class
+        //We are passing ResponseWrapper because we want to customize the json object
+        return ResponseEntity.ok(new ResponseWrapper("All users retrieved with success", userService.listAllUsers(), HttpStatus.OK));
+        // difference between .ok -- postman upper corner and HttpStatus.OK --in the body
+    }
+    @PostMapping
+    @RolesAllowed("Admin")
+    @Operation
+public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO userDTO){
+        userService.save(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("user is successfully created",HttpStatus.OK));
+
+
+}
 }
